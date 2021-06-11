eeac19e3b6408237113d57226c48e540f7c6460c
Augusto Silva <gutim160@gmail.com>
2021-06-11 09:59:34 -0300

    Exportando projeto

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" class="logo" src=".\assets\coindesk.png">
+    <CoindDesk id="con"/>
+  </div>
+</template>
+
+<script>
+import CoindDesk from './components/CoindDesk.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CoindDesk
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  width: 600px;
+  height: 600px;
+}
+</style>
---
+++ src/components/CoindDesk.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>Preços do Bitcoin</h1>
+    <div v-for="currency in info"
+      class="currency" :key="currency">
+      {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios').default
+export default {
+  el: "#app",
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(response => (this.info = response.data.bpi))
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div {
+    display: block;
+    text-align: center;
+  }
+  #app {
+    margin-top: 20px;
+    width: 300px;
+    padding: 0 40px 40px;
+    background: #2F242C;
+    border-radius: 5px;
+    color: #B3BFB8;
+  }
+
+</style>
